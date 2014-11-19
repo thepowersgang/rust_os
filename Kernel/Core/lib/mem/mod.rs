d98f8851afb255c0453d34fa8543d7a7f58a86b2
John Hodge <tpg@mutabah.net>
2014-11-19 10:43:56 +0800

    Kernel/Box - Use 'where' for parameter bound

--- Kernel/Core/lib/mem/mod.rs
+++ Kernel/Core/lib/mem/mod.rs
@@ -6,7 +6,9 @@ mod rc;
 #[lang = "owned_box"]
 pub struct Box<T>(*mut T);
 
-impl<T: ::core::fmt::Show> ::core::fmt::Show for Box<T>
+impl<T> ::core::fmt::Show for Box<T>
+where
+	T: ::core::fmt::Show
 {
 	fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::result::Result<(),::core::fmt::FormatError>
 	{
