6a8446779d74f66587668c5ea4951145d3411e7b
John Hodge <tpg@mutabah.net>
2014-11-19 10:44:15 +0800

    Kernel/DevManager - Continued design work, hacked up device register

--- Kernel/Core/device_manager.rs
+++ Kernel/Core/device_manager.rs
@@ -16,14 +16,21 @@ pub enum IOBinding
 	IOBindIO(u16,u16),
 }
 
-trait BusManager
+pub trait BusManager
 {
+	fn get_attr_names() -> &[&str];
 }
 
 pub trait BusDevice
 {
 	fn get_attr(name: &str) -> u32;
+	fn set_power(state: bool);	// TODO: Power state enum for Off,Standby,Low,On
 	fn bind_io(block_id: uint) -> IOBinding;
+}
+
+pub trait Driver
+{
+	
 }
 
 pub trait DriverInstance
@@ -43,15 +50,46 @@ struct Bus
 	devices: Vec<Device>,
 }
 
+#[allow(non_upper_case_globals)]
 static s_root_busses: Mutex<Queue<Bus>> = mutex_init!(queue_init!());
+
+//#[allow(non_upper_case_globals)]
+//static s_driver_list: Mutex<Vec<&'static Driver>> = mutex_init!( Vec::new() );
 
 fn init()
 {
 	// Do nothing!
 }
 
-//pub fn register_bus(devices: Vec<Box<BusDevice+'static>>)
-//{
-//}
+pub fn register_bus(manager: &'static BusManager+'static, devices: Vec<Box<BusDevice+'static>>)
+{
+	let bus = Bus {
+		manager: manager,
+		devices: devices.into_iter().map(|d| Device {
+			driver: find_driver(&*d),
+			bus_dev: d,
+			attribs: Vec::new(),
+			}).collect(),
+		};
+	s_root_busses.lock().push(bus);
+}
+
+pub fn register_driver(driver: &'static Driver+'static)
+{
+	
+}
+
+fn find_driver(bus_dev: &BusDevice) -> Option<Box<DriverInstance+'static>>
+{
+	/*
+	for driver in s_driver_list.lock().items()
+	{
+		if driver.handles(bus_dev) {
+			return Some( driver.bind(bus_dev) );
+		}
+	}
+	*/
+	None
+}
 
 // vim: ft=rust
