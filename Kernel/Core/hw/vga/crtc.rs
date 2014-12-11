8dffbf0983b57e089933977d62c40fe0c9dab377
John Hodge <tpg@mutabah.net>
2014-12-11 14:02:28 +0800

    HW/VGA - Split CRCT registers out into a separate file

---
+++ Kernel/Core/hw/vga/crtc.rs
@@ -0,0 +1,177 @@
+// "Tifflin" Kernel
+// - By John Hodge (thePowersGang)
+//
+// Core/hw/vga/crtc.rs
+// - VGA CRTC Handler
+//
+use _common::*;
+use lib::UintBits;
+
+pub struct CrtcRegs
+{
+	regs: [u8, ..0x20],
+}
+
+pub const PIX_PER_CHAR: uint = 16u;
+
+impl CrtcRegs
+{
+	pub fn load(base: u16) -> CrtcRegs
+	{
+		let mut rv = CrtcRegs {
+			regs: [0, ..0x20],
+			};
+		rv.read(base);
+		rv
+	}
+	
+	// CR0: H Total
+	pub fn set_h_total(&mut self, val: u16)
+	{
+		assert!(val <= 0xFF);
+		self.regs[0] = (val & 0xFF) as u8;
+	}
+	// CR1: H Display End
+	pub fn set_h_disp_end(&mut self, val: u16)
+	{
+		assert!(val <= 0xFF);
+		self.regs[1] = (val & 0xFF) as u8;
+	}
+	// CR2: H Blank Start
+	pub fn set_h_blank_start(&mut self, val: u16)
+	{
+		assert!(val <= 0xFF);
+		self.regs[2] = (val & 0xFF) as u8;
+	}
+	// CR3: H Blank Length
+	pub fn set_h_blank_len(&mut self, val: u16)
+	{
+		assert!(val <= 0x3F);
+		self.regs[3] &= !(0x1F << 0);
+		self.regs[3] |= (val & 0x1F) as u8;
+		self.regs[0x5] &= !(1 << 7);
+		self.regs[0x5] |= val.bit(5) as u8 << 7;
+	}
+	// CR4: H Sync Start
+	pub fn set_h_sync_start(&mut self, val: u16)
+	{
+		assert!(val <= 0x1FF);
+		self.regs[4] = (val & 0xFF) as u8;
+		self.regs[0x1A] &= !(1 << 4);
+		self.regs[0x1A] |= val.bit(8) as u8 << 4;
+	}
+	// CR5: H Sync End
+	pub fn set_h_sync_end(&mut self, val: u16)
+	{
+		assert!(val <= 0x1F);
+		self.regs[5] &= !(0x1F);
+		self.regs[5] &= (val & 0x1F) as u8;
+	}
+	// CR6: V Total
+	pub fn set_v_total(&mut self, val: u16)
+	{
+		assert!(val <= 0x3FF);
+		self.regs[6] = (val & 0xFF) as u8;
+		self.regs[7] &= !( (1 << 0) | (1 << 5) );
+		self.regs[7] |= (val.bit(8) << 0 | val.bit(9) << 5) as u8;
+	}
+	// CR12: V Display End
+	pub fn set_v_disp_end(&mut self, val: u16)
+	{
+		assert!(val <= 0x3FF);
+		self.regs[0x12] = (val & 0xFF) as u8;
+		// CR7[1,6] := val[8,9]
+		self.regs[0x07] &= !( 1 << 1 | 1 << 6 );
+		self.regs[0x07] |= (val.bit(8) << 1 | val.bit(9) << 6) as u8;
+	}
+	// CR15: V Blank Start
+	pub fn set_v_blank_start(&mut self, val: u16)
+	{
+		assert!(val <= 0x3FF);
+		self.regs[0x15] = (val & 0xFF) as u8;
+		self.regs[0x07] &= !( 1 << 3 );
+		self.regs[0x07] |= val.bit(8) as u8 << 3;
+		self.regs[0x09] &= !( 1 << 5 );
+		self.regs[0x09] |= val.bit(9) as u8 << 5;
+	}
+	// CR16: V Blank End
+	pub fn set_v_blank_end(&mut self, val: u16)
+	{
+		assert!(val <= 0x3FF);
+		self.regs[0x16] = (val & 0xFF) as u8;
+		self.regs[0x1A] &= !( 3 << 6 );
+		self.regs[0x1A] |= val.bits(8,9) as u8 << 6;
+	}
+	// CR10: V Sync Start
+	pub fn set_v_sync_start(&mut self, val: u16)
+	{
+		assert!(val <= 0x3FF);
+		self.regs[0x10] = (val & 0xFF) as u8;
+		self.regs[0x07] &= !( 1 << 2 | 1 << 7 );
+		self.regs[0x07] |= (val.bit(8) << 2 | val.bit(9) << 7) as u8;
+	}
+	// CR11: V Sync End
+	pub fn set_v_sync_end(&mut self, val: u16)
+	{
+		assert!(val <= 0xF);
+		self.regs[0x11] &= !( 0xF );
+		self.regs[0x11] |= (val & 0xF) as u8;
+	}
+	// CR18: Line Compare - The scanline where ScreenA finishes (And ScreenB starts)
+	pub fn set_line_compare(&mut self, val: u16)
+	{
+		assert!(val < 0x3FF);
+		self.regs[0x18] = (val & 0xFF) as u8;
+		self.regs[0x07] &= !( 1 << 4 );
+		self.regs[0x07] |= val.bit(8) as u8 << 4;
+		self.regs[0x09] &= !( 1 << 6 );
+		self.regs[0x09] |= val.bit(9) as u8 << 6;
+	}
+	// CR13: Offset (vertical scrolling)
+	pub fn set_offset(&mut self, val: u16)
+	{
+		assert!(val < 0x1FF);
+		self.regs[0x13] = (val & 0xFF) as u8;
+		self.regs[0x1B] &= !( 1 << 4 );
+		self.regs[0x1B] |= val.bit(8) as u8 << 4;
+	}
+	
+	// CR8: Byte Pan
+	pub fn set_byte_pan(&mut self, val: u8)
+	{
+		self.regs[8] &= !( 3 << 5 );
+		self.regs[8] |= (val & 3) << 5
+	}
+
+	// CRC/CRD: Screen Start
+	pub fn set_screen_start(&mut self, val: u16)
+	{
+		self.regs[0xC] = (val >> 8) as u8;
+		self.regs[0xD] = (val & 0xFF) as u8;
+	}
+	
+	
+	fn read(&mut self, base: u16)
+	{
+		for (idx,val) in self.regs.iter_mut().enumerate()
+		{
+			unsafe {
+				::arch::x86_io::outb(base + 0, idx as u8);
+				*val = ::arch::x86_io::inb(base + 1);
+			}
+		}
+	}
+
+	pub fn commit(&mut self, base: u16)
+	{
+		for (idx,val) in self.regs.iter().enumerate()
+		{
+			unsafe {
+				::arch::x86_io::outb(base + 0, idx as u8);
+				::arch::x86_io::outb(base + 1, *val);
+			}
+		}
+	}
+}
+
+// vim: ft=rust
--- Kernel/Core/hw/vga/mod.rs
+++ Kernel/Core/hw/vga/mod.rs
@@ -7,9 +7,10 @@
 // TODO: Move this to an external link-time included module
 use _common::*;
 use metadevs::video::{Framebuffer,Rect};
-use lib::UintBits;
 
 module_define!(VGA, [DeviceManager, Video], init)
+
+mod crtc;
 
 struct VgaPciDriver;
 //struct VgaStaticDriver;
@@ -24,11 +25,7 @@ struct VgaFramebuffer
 {
 	io_base: u16,
 	window: ::memory::virt::AllocHandle,
-	crtc: CrtcRegs,
-}
-struct CrtcRegs
-{
-	regs: [u8, ..0x20],
+	crtc: crtc::CrtcRegs,
 }
 struct CrtcAttrs
 {
@@ -43,8 +40,6 @@ struct CrtcAttrs
 	v_back_porch: u16,
 	v_sync_len: u16,
 }
-
-const PIX_PER_CHAR: uint = 16u;
 
 #[allow(non_upper_case_globals)]
 static s_vga_pci_driver: VgaPciDriver = VgaPciDriver;
@@ -105,7 +100,7 @@ impl VgaFramebuffer
 		let mut rv = VgaFramebuffer {
 			io_base: base,
 			window: ::memory::virt::map_hw_rw(0xA0000, (0xC0-0xA0), module_path!()).unwrap(),
-			crtc: CrtcRegs::load(base + 0x24),	// Colour CRTC regs
+			crtc: crtc::CrtcRegs::load(base + 0x24),	// Colour CRTC regs
 			};
 		
 		// 320x240 @60Hz
@@ -116,6 +111,7 @@ impl VgaFramebuffer
 	
 	fn set_crtc(&mut self, attrs: CrtcAttrs)
 	{
+		use self::crtc::PIX_PER_CHAR;
 		self.crtc.set_line_compare(0);	// Entire screen is Screen B
 		self.crtc.set_screen_start(0);	// Screen A starts at offset 0 (making A and B functionally equal)
 		self.crtc.set_byte_pan(0);	// Byte pan = 0, no horizontal offset
@@ -148,166 +144,6 @@ impl VgaFramebuffer
 		self.crtc.set_v_sync_end(v_sync_end);
 		
 		self.crtc.commit(self.io_base + 0x24);
-	}
-}
-
-impl CrtcRegs
-{
-	fn load(base: u16) -> CrtcRegs
-	{
-		let mut rv = CrtcRegs {
-			regs: [0, ..0x20],
-			};
-		rv.read(base);
-		rv
-	}
-	
-	// CR0: H Total
-	pub fn set_h_total(&mut self, val: u16)
-	{
-		assert!(val <= 0xFF);
-		self.regs[0] = (val & 0xFF) as u8;
-	}
-	// CR1: H Display End
-	pub fn set_h_disp_end(&mut self, val: u16)
-	{
-		assert!(val <= 0xFF);
-		self.regs[1] = (val & 0xFF) as u8;
-	}
-	// CR2: H Blank Start
-	pub fn set_h_blank_start(&mut self, val: u16)
-	{
-		assert!(val <= 0xFF);
-		self.regs[2] = (val & 0xFF) as u8;
-	}
-	// CR3: H Blank Length
-	pub fn set_h_blank_len(&mut self, val: u16)
-	{
-		assert!(val <= 0x3F);
-		self.regs[3] &= !(0x1F << 0);
-		self.regs[3] |= (val & 0x1F) as u8;
-		self.regs[0x5] &= !(1 << 7);
-		self.regs[0x5] |= val.bit(5) as u8 << 7;
-	}
-	// CR4: H Sync Start
-	pub fn set_h_sync_start(&mut self, val: u16)
-	{
-		assert!(val <= 0x1FF);
-		self.regs[4] = (val & 0xFF) as u8;
-		self.regs[0x1A] &= !(1 << 4);
-		self.regs[0x1A] |= val.bit(8) as u8 << 4;
-	}
-	// CR5: H Sync End
-	pub fn set_h_sync_end(&mut self, val: u16)
-	{
-		assert!(val <= 0x1F);
-		self.regs[5] &= !(0x1F);
-		self.regs[5] &= (val & 0x1F) as u8;
-	}
-	// CR6: V Total
-	pub fn set_v_total(&mut self, val: u16)
-	{
-		assert!(val <= 0x3FF);
-		self.regs[6] = (val & 0xFF) as u8;
-		self.regs[7] &= !( (1 << 0) | (1 << 5) );
-		self.regs[7] |= (val.bit(8) << 0 | val.bit(9) << 5) as u8;
-	}
-	// CR12: V Display End
-	pub fn set_v_disp_end(&mut self, val: u16)
-	{
-		assert!(val <= 0x3FF);
-		self.regs[0x12] = (val & 0xFF) as u8;
-		// CR7[1,6] := val[8,9]
-		self.regs[0x07] &= !( 1 << 1 | 1 << 6 );
-		self.regs[0x07] |= (val.bit(8) << 1 | val.bit(9) << 6) as u8;
-	}
-	// CR15: V Blank Start
-	pub fn set_v_blank_start(&mut self, val: u16)
-	{
-		assert!(val <= 0x3FF);
-		self.regs[0x15] = (val & 0xFF) as u8;
-		self.regs[0x07] &= !( 1 << 3 );
-		self.regs[0x07] |= val.bit(8) as u8 << 3;
-		self.regs[0x09] &= !( 1 << 5 );
-		self.regs[0x09] |= val.bit(9) as u8 << 5;
-	}
-	// CR16: V Blank End
-	pub fn set_v_blank_end(&mut self, val: u16)
-	{
-		assert!(val <= 0x3FF);
-		self.regs[0x16] = (val & 0xFF) as u8;
-		self.regs[0x1A] &= !( 3 << 6 );
-		self.regs[0x1A] |= val.bits(8,9) as u8 << 6;
-	}
-	// CR10: V Sync Start
-	pub fn set_v_sync_start(&mut self, val: u16)
-	{
-		assert!(val <= 0x3FF);
-		self.regs[0x10] = (val & 0xFF) as u8;
-		self.regs[0x07] &= !( 1 << 2 | 1 << 7 );
-		self.regs[0x07] |= (val.bit(8) << 2 | val.bit(9) << 7) as u8;
-	}
-	// CR11: V Sync End
-	pub fn set_v_sync_end(&mut self, val: u16)
-	{
-		assert!(val <= 0xF);
-		self.regs[0x11] &= !( 0xF );
-		self.regs[0x11] |= (val & 0xF) as u8;
-	}
-	// CR18: Line Compare - The scanline where ScreenA finishes (And ScreenB starts)
-	pub fn set_line_compare(&mut self, val: u16)
-	{
-		assert!(val < 0x3FF);
-		self.regs[0x18] = (val & 0xFF) as u8;
-		self.regs[0x07] &= !( 1 << 4 );
-		self.regs[0x07] |= val.bit(8) as u8 << 4;
-		self.regs[0x09] &= !( 1 << 6 );
-		self.regs[0x09] |= val.bit(9) as u8 << 6;
-	}
-	// CR13: Offset (vertical scrolling)
-	pub fn set_offset(&mut self, val: u16)
-	{
-		assert!(val < 0x1FF);
-		self.regs[0x13] = (val & 0xFF) as u8;
-		self.regs[0x1B] &= !( 1 << 4 );
-		self.regs[0x1B] |= val.bit(8) as u8 << 4;
-	}
-	
-	// CR8: Byte Pan
-	pub fn set_byte_pan(&mut self, val: u8)
-	{
-		self.regs[8] &= !( 3 << 5 );
-		self.regs[8] |= (val & 3) << 5
-	}
-
-	// CRC/CRD: Screen Start
-	pub fn set_screen_start(&mut self, val: u16)
-	{
-		self.regs[0xC] = (val >> 8) as u8;
-		self.regs[0xD] = (val & 0xFF) as u8;
-	}
-	
-	
-	fn read(&mut self, base: u16)
-	{
-		for (idx,val) in self.regs.iter_mut().enumerate()
-		{
-			unsafe {
-				::arch::x86_io::outb(base + 0, idx as u8);
-				*val = ::arch::x86_io::inb(base + 1);
-			}
-		}
-	}
-
-	pub fn commit(&mut self, base: u16)
-	{
-		for (idx,val) in self.regs.iter().enumerate()
-		{
-			unsafe {
-				::arch::x86_io::outb(base + 0, idx as u8);
-				::arch::x86_io::outb(base + 1, *val);
-			}
-		}
 	}
 }
 
