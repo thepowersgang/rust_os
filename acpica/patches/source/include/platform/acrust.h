3c3a1213d8006ac85d6b8ed4612c134e57ce589d
John Hodge <tpg@mutabah.net>
2015-04-26 17:48:18 +0800

    ACPICA - Rough patches

---
+++ acpica/patches/source/include/platform/acrust.h
@@ -0,0 +1,20 @@
+#ifndef __ACRUST_H__
+#define __ACRUST_H__
+
+
+/* Building for rust bindings uses GCC */
+
+#include "acgcc.h"
+
+#define COMPILER_DEPENDENT_INT64        int64_t
+#define COMPILER_DEPENDENT_UINT64       uint64_t
+
+#define ACPI_UINTPTR_T      uintptr_t
+
+#define ACPI_USE_DO_WHILE_0
+#define ACPI_USE_LOCAL_CACHE
+#define ACPI_USE_NATIVE_DIVIDE
+#define ACPI_USE_SYSTEM_CLIBRARY
+
+#endif
+
