81a727d57f661ee2c5fa6a5ceee2c0ce15bf027f
John Hodge <tpg@mutabah.net>
2015-04-26 17:59:08 +0800

    ACPICA - Compiles, but evilly (only for 64-bit)

--- acpica/patches/source/include/platform/acrust.h
+++ acpica/patches/source/include/platform/acrust.h
@@ -1,10 +1,14 @@
 #ifndef __ACRUST_H__
 #define __ACRUST_H__
 
-
 /* Building for rust bindings uses GCC */
 
 #include "acgcc.h"
+#include <stdint.h>
+#include <stdarg.h>
+
+// TODO: Handle this being different
+#define ACPI_MACHINE_WIDTH	64
 
 #define COMPILER_DEPENDENT_INT64        int64_t
 #define COMPILER_DEPENDENT_UINT64       uint64_t
