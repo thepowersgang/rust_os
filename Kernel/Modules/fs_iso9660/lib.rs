f9ff2c7490ff119f075b01907e1f77fd053344e5
John Hodge <tpg@mutabah.net>
2015-06-01 21:26:49 +0800

    Kernel/ISO9660 - Implement mount

--- Kernel/Modules/fs_iso9660/lib.rs
+++ Kernel/Modules/fs_iso9660/lib.rs
@@ -11,8 +11,11 @@ use kernel::prelude::*;
 use kernel::vfs::{self, mount, node};
 use kernel::metadevs::storage::{self,VolumeHandle,SizePrinter};
 use kernel::lib::mem::aref::{ArefInner,ArefBorrow};
+use kernel::lib::byteorder::{ByteOrder,LittleEndian};
 
 module_define!{FS_ISO9660, [VFS], init}
+
+//mod ondisk;
 
 struct Driver;
 static S_DRIVER: Driver = Driver;
@@ -26,6 +29,9 @@ impl ::core::ops::Deref for Instance {
 struct InstanceInner
 {
 	pv: VolumeHandle,
+	lb_size: usize,
+	root_lba: u32,
+	root_size: u32,
 }
 
 fn init()
@@ -52,7 +58,58 @@ impl mount::Driver for Driver
 		}
 	}
 	fn mount(&self, vol: VolumeHandle) -> vfs::Result<Box<mount::Filesystem>> {
-		todo!("mount");
+		if 2048 % vol.block_size() != 0 {
+			return Err( vfs::Error::Unknown("Can't mount ISO9660 with sector size not a factor of 2048"/*, vol.block_size()*/) );
+		}
+		let scale = 2048 / vol.block_size();
+		
+		let mut block: Vec<u8> = (0 .. 2048).map(|_|0).collect();
+		for sector in (16 .. )
+		{
+			try!(vol.read_blocks((sector*scale) as u64, &mut block));
+			if &block[1..6] != b"CD001" {
+				return Err( vfs::Error::Unknown("Invalid volume descriptor present") );
+			}
+			else if block[0] == 255 {
+				return Err( vfs::Error::Unknown("Can't find ISO9660 primary volume descriptor") );
+			}
+			else if block[0] == 0x01 {
+				// Found it!
+				break ;
+			}
+			else {
+				// Try the next one
+			}
+		}
+		::kernel::logging::hex_dump("ISO966 PVD", &block);
+		
+		// Obtain the logical block size (different from medium sector size)
+		let lb_size = LittleEndian::read_u16(&block[128..]);
+		// Extract the root directory entry
+		// - We want the LBA and byte length
+		let root_lba  = LittleEndian::read_u32(&block[156+ 2..]);
+		let root_size = LittleEndian::read_u32(&block[156+10..]);
+		
+		Ok( Box::new( Instance(
+			unsafe { ArefInner::new( InstanceInner {
+				pv: vol,
+				lb_size: lb_size as usize,
+				root_lba: root_lba,
+				root_size: root_size,
+				} ) }
+			) ) )
 	}
 }
+
+impl mount::Filesystem for Instance
+{
+	fn root_inode(&self) -> node::InodeId {
+		todo!("Instance::root_inode");
+	}
+	fn get_node_by_inode(&self, id: node::InodeId) -> Option<node::Node> {
+		todo!("Instance::get_node_by_inode");
+	}
+
+}
+
 
