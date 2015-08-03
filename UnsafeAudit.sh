6f98a54e4900dc93bb49ad80c827bae3e97f82d9
John Hodge <tpg@mutabah.net>
2015-08-03 18:28:41 +0800

    Tidy up UnsafeAudit.sh

--- UnsafeAudit.sh
+++ UnsafeAudit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 grep --include=*.rs 'unsafe *[{$]' -nrI Kernel/Core/ Kernel/Modules/ Usermode/ -B 1 \
-	| awk 'BEGIN{issafe = 0; count=0;} { if(match($0, /SAFE:/) != 0) {issafe = 1} else { if(issafe == 0) { if(match($0, /unsafe/)) {count += 1;} print $0} else {}; issafe=0; } } END{ print count; exit (count==0?0:1); }' \
-	| uniq
+	| awk 'BEGIN{print "--"; issafe = 0; count=0;} { if(match($0, /SAFE:/) != 0) {issafe = 1} else { if(issafe == 0) { if(match($0, /unsafe/)) {count += 1;} print $0} else {}; issafe=0; } } END{ exit (count==0?0:1); }' \
+	| uniq | tail -n +2
 exit ${PIPESTATUS[1]}
 #grep '[^(//)]*unsafe [^(fn)]' -nrI Kernel/Core/ Kernel/Modules/ -B 1 | awk 'BEGIN {issafe = 0;} { if(match($0, /SAFE:/) != 0) { issafe = 1 } else { if(issafe == 0) {print $0} else {}; issafe=0; } }' | uniq
