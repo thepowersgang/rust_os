7945f0d42b7f81f9740a328ad1c1d33547c7e671
John Hodge <tpg@mutabah.net>
2015-09-21 15:04:14 +0800

    Update UnsafeAudit.sh to correcly handle unsafe blocks with the `{` on the next line

--- UnsafeAudit.sh
+++ UnsafeAudit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep --include=*.rs 'unsafe *[{$]' -nrI Kernel/Core/ Kernel/Modules/ Usermode/ -B 1 \
+grep --include=*.rs 'unsafe *{\|unsafe */\|unsafe *$' -nrI Kernel/Core/ Kernel/Modules/ Usermode/ -B 1 \
 	| awk 'BEGIN{print "--"; issafe = 0; count=0;} { if(match($0, /SAFE:/) != 0) {issafe = 1} else { if(issafe == 0) { if(match($0, /unsafe/)) {count += 1;} print $0} else {}; issafe=0; } } END{ exit (count==0?0:1); }' \
 	| uniq | tail -n +2
 exit ${PIPESTATUS[1]}
