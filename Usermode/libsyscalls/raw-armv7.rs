7a1b834f299a92128ec38b6bddf34dbc60cc4fc3
John Hodge <tpg@mutabah.net>
2015-10-11 12:43:29 +0800

    Kernel/ARMv7 - System call support

--- Usermode/libsyscalls/raw-armv7.rs
+++ Usermode/libsyscalls/raw-armv7.rs
@@ -5,7 +5,7 @@
 			let rv_h: usize;
 			asm!("swi #0"
 				: "={r0}" (rv_l), "={r1}" (rv_h)
-				: "{r0}" ($id as usize) $(, $reg ($val as usize))*
+				: "{r12}" ($id as usize) $(, $reg ($val as usize))*
 				:
 				: "volatile"
 				);
@@ -17,20 +17,20 @@
 		syscall_a!(id, )
 	}
 	pub unsafe fn syscall_1(id: u32, a1: usize) -> u64 {
-		syscall_a!(id, "{r1}"=a1)
+		syscall_a!(id, "{r0}"=a1)
 	}
 	pub unsafe fn syscall_2(id: u32, a1: usize, a2: usize) -> u64 {
-		syscall_a!(id, "{r1}"=a1, "{r2}"=a2)
+		syscall_a!(id, "{r0}"=a1, "{r1}"=a2)
 	}
 	pub unsafe fn syscall_3(id: u32, a1: usize, a2: usize, a3: usize) -> u64 {
-		syscall_a!(id, "{r1}"=a1, "{r2}"=a2, "{r3}"=a3)
+		syscall_a!(id, "{r0}"=a1, "{r1}"=a2, "{r2}"=a3)
 	}
 	pub unsafe fn syscall_4(id: u32, a1: usize, a2: usize, a3: usize, a4: usize) -> u64 {
-		syscall_a!(id, "{r1}"=a1, "{r2}"=a2, "{r3}"=a3, "{r4}"=a4)
+		syscall_a!(id, "{r0}"=a1, "{r1}"=a2, "{r2}"=a3, "{r3}"=a4)
 	}
 	pub unsafe fn syscall_5(id: u32, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) -> u64 {
-		syscall_a!(id, "{r1}"=a1, "{r2}"=a2, "{r3}"=a3, "{r4}"=a4, "{r5}"=a5)
+		syscall_a!(id, "{r0}"=a1, "{r1}"=a2, "{r2}"=a3, "{r3}"=a4, "{r4}"=a5)
 	}
 	pub unsafe fn syscall_6(id: u32, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize, a6: usize) -> u64 {
-		syscall_a!(id, "{r1}"=a1, "{r2}"=a2, "{r3}"=a3, "{r4}"=a4, "{r5}"=a5, "{r6}"=a6)
+		syscall_a!(id, "{r0}"=a1, "{r1}"=a2, "{r2}"=a3, "{r3}"=a4, "{r4}"=a5, "{r5}"=a6)
 	}
