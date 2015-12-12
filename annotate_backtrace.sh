f5f5140baf9bc12205b574e571cf67aafea775f8
John Hodge <tpg@mutabah.net>
2015-12-12 19:24:07 +0800

    Update annotate_backtrace script so it works with userland dumps

--- annotate_backtrace.sh
+++ annotate_backtrace.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-DSM=Kernel/bin/kernel-amd64.bin.dsm
-echo "- Disassembling kernel"
-objdump -S Kernel/bin/kernel-amd64.bin > $DSM
+BACKTRACE=$1
+if [[ $# -ge 2 ]]; then
+	DSM=$2
+else
+	BINFILE=Kernel/bin/kernel-amd64.bin
+	echo "- Disassembling kernel"
+	objdump -S Kernel/bin/kernel-amd64.bin > ${BINFILE}.dsm
+	DSM=${BINFILE}.dsm
+fi
+
 echo "- Annotated backtrace"
-BT=$(echo $1 | sed 's/.*: //' | sed 's/ > / /g' | sed 's/0x//g')
+BT=$(echo ${BACKTRACE} | sed 's/.*: //' | sed 's/ > / /g' | sed 's/0x//g')
 for addr in $BT; do
 	echo "-- $addr"
-	grep '>:$\|^'$addr':' $DSM | grep ^$addr -B 1
+	grep '>:$\|^ *'$addr':' $DSM | grep '^ *'$addr -B 1
 done
 
 
