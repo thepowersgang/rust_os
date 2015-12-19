503b3e6838c428f13f5faf143449c4a08163e679
John Hodge <tpg@mutabah.net>
2015-12-19 08:07:58 +0800

    Kernel/AMD64 - Update port IO

--- Kernel/Core/arch/amd64/x86_io.rs
+++ Kernel/Core/arch/amd64/x86_io.rs
@@ -8,39 +8,39 @@
 #[inline]
 pub unsafe fn inb(port: u16) -> u8 {
 	let ret : u8;
-	asm!("inb %dx, %al" : "={ax}"(ret) : "{dx}"(port) : : "volatile");
+	asm!("inb $1, $0" : "={ax}"(ret) : "{dx}N"(port) : : "volatile");
 	return ret;
 }
 /// Write a single byte
 #[inline]
 pub unsafe fn outb(port: u16, val: u8) {
-	asm!("outb %al, %dx" : : "{dx}"(port), "{al}"(val) : : "volatile");
+	asm!("outb $1, $0" : : "{dx}N"(port), "{al}"(val) : : "volatile");
 }
 
 /// Read a 16-bit word
 #[inline]
 pub unsafe fn inw(port: u16) -> u16 {
 	let ret : u16;
-	asm!("inw %dx, %ax" : "={ax}"(ret) : "{dx}"(port) : : "volatile");
+	asm!("inw $1, $0" : "={ax}"(ret) : "{dx}N"(port) : : "volatile");
 	return ret;
 }
 /// Write a 16-bit word
 #[inline]
 pub unsafe fn outw(port: u16, val: u16) {
-	asm!("outw %ax, %dx" : : "{dx}"(port), "{ax}"(val) : : "volatile");
+	asm!("outw %ax, $0" : : "{dx}N"(port), "{ax}"(val) : : "volatile");
 }
 
 /// Read a 32-bit long/double-word
 #[inline]
 pub unsafe fn inl(port: u16) -> u32 {
 	let ret : u32;
-	asm!("inl %dx, %eax" : "={eax}"(ret) : "{dx}"(port) : : "volatile");
+	asm!("inl $1, $0" : "={eax}"(ret) : "{dx}N"(port) : : "volatile");
 	return ret;
 }
 /// Write a 32-bit long/double-word
 #[inline]
 pub unsafe fn outl(port: u16, val: u32) {
-	asm!("outl %eax, %dx" : : "{dx}"(port), "{eax}"(val) : : "volatile");
+	asm!("outl %eax, $0" : : "{dx}N"(port), "{eax}"(val) : : "volatile");
 }
 
 // vim: ft=rust
