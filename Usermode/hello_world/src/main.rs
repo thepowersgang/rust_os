b3f2e7fbb5ad9d5c0f4ea354a32fc844379176ba
John Hodge <tpg@mutabah.net>
2015-12-28 22:14:49 +0800

    Add hacky hello_world userland app (uses kernel log)

---
+++ Usermode/hello_world/src/main.rs
@@ -0,0 +1,18 @@
+#![feature(start)]
+#![no_std]
+
+#[macro_use]
+extern crate syscalls;
+extern crate std_rt;
+
+#[start]
+fn main(_: isize, _: *const *const u8) -> isize {
+	::syscalls::log_write("Hello World!");
+	0
+}
+
+#[no_mangle]
+pub extern "C" fn register_arguments() {
+	// Does nothing
+}
+
