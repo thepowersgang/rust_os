2c96e25bb8e7cd9427024796b97f9e103c541af5
John Hodge <tpg@mutabah.net>
2016-02-13 19:07:23 +0800

    Kernel - Commit forgotten file

---
+++ Kernel/Core/memory/user.rs
@@ -0,0 +1,20 @@
+/// Access to user-controlled memory
+///
+
+pub fn read<T: ::lib::POD>(addr: usize) -> Result<T, ()> {
+	assert!( ::core::mem::size_of::<T>() < ::PAGE_SIZE );
+	let ptr = addr as *const T;
+	if ! ::memory::buf_valid(ptr as *const (), ::core::mem::size_of::<T>()) {
+		Err( () )
+	}
+	else if addr % ::core::mem::align_of::<T>() != 0 {
+		Err( () )
+	}
+	else {
+		// TODO: XXX Handle potential for user to alter the AS during this
+		// SAFE: (Assuming single-thread) Alignment and validity checked
+		unsafe {
+			Ok( ::core::ptr::read(addr as *const T) )
+		}
+	}
+}
