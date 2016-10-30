e012176a732315e999c26d4a25f25b8f500a79e6
John Hodge <tpg@mutabah.net>
2016-10-30 13:18:37 +0800

    Kernel - Boxed lang items

--- Kernel/Core/lib/mem/boxed.rs
+++ Kernel/Core/lib/mem/boxed.rs
@@ -3,14 +3,12 @@
 //
 // Core/lib/mem/mod.rs
 //! Owned dynamic allocation (box)
-use core::marker::{Sized,Unsize};
-use core::ops::{CoerceUnsized};
-use core::{ops,fmt};
+use core::{mem,ops,fmt,marker};
 
 #[lang = "owned_box"]
 pub struct Box<T: ?Sized>(::core::ptr::Unique<T>);
 
-impl<T: ?Sized + Unsize<U>, U: ?Sized> CoerceUnsized<Box<U>> for Box<T> { }
+impl<T: ?Sized + marker::Unsize<U>, U: ?Sized> ops::CoerceUnsized<Box<U>> for Box<T> { }
 
 impl<T> Box<T>
 {
@@ -48,6 +46,57 @@ impl<T: ?Sized> Box<T>
 			}
 		}
 		::core::mem::forget(this);
+	}
+}
+
+impl<T> ops::Boxed for Box<T> {
+	type Data = T;
+	type Place = IntermediateBox<T>;
+	unsafe fn finalize(b: IntermediateBox<T>) -> Box<T> {
+		let p = b.ptr;
+		::core::mem::forget(b);
+		// TODO: Unsized?
+		::core::mem::transmute(p)
+	}
+}
+pub struct IntermediateBox<T: ?Sized> {
+	ptr: *mut u8,
+	size: usize,
+	align: usize,
+	marker: marker::PhantomData<*mut T>,
+}
+impl<T: ?Sized> Drop for IntermediateBox<T> {
+	fn drop(&mut self) {
+		if self.size > 0 {
+			// SAFE: Pointer is valid, no need to drop
+			unsafe { ::memory::heap::dealloc_raw(self.ptr as *mut (), self.size, self.align) }
+		}
+	}
+}
+impl<T> ops::BoxPlace<T> for IntermediateBox<T> {
+	fn make_place() -> IntermediateBox<T> {
+		let size = mem::size_of::<T>();
+		let align = mem::align_of::<T>();
+		
+		let ptr = if size == 0 {
+				::memory::heap::ZERO_ALLOC as *mut u8
+			}
+			else {
+				// SAFE: Tell me again, why is allo unsafe?
+				unsafe { ::memory::heap::alloc_raw(size, align) as *mut u8 }
+			};
+
+		IntermediateBox {
+			ptr: ptr,
+			size: size,
+			align: align,
+			marker: marker::PhantomData,
+			}
+	}
+}
+impl<T> ops::Place<T> for IntermediateBox<T> {
+	fn pointer(&mut self) -> *mut T {
+		self.ptr as *mut T
 	}
 }
 
