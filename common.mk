3ecccdb5fbee4b4d4e795ccb538ff65a3fe59054
John Hodge <tpg@mutabah.net>
2017-07-28 23:53:44 +0800

    Makefile - Don't extract a now-removed directory from the rustc tarball

--- common.mk
+++ common.mk
@@ -33,5 +33,5 @@ RUSTDOC := $(call fn_rustcmd,rustdoc)
 CARGO := $(call fn_rustcmd,cargo)
 
 $(patsubst %,../rustc_src/lib%/lib.rs,core collections std_unicode alloc): ../rustc-nightly-src.tar.gz
-	tar -C .. -xmf $< --wildcards 'rustc-nightly-src/src/lib*' rustc-nightly-src/src/driver rustc-nightly-src/src/rt --transform 's~^rustc-nightly-src/src/~rustc_src/~'
+	tar -C .. -xmf $< --wildcards 'rustc-nightly-src/src/lib*' rustc-nightly-src/src/rt --transform 's~^rustc-nightly-src/src/~rustc_src/~'
 
