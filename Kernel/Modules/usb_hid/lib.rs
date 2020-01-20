5b8982d42dc0caab6023de6afeae430f3e64205f
John Hodge <tpg@mutabah.net>
2020-01-20 23:04:49 +0800

    USB HID - Move report parser to its own file, structre for collection checking

--- Kernel/Modules/usb_hid/lib.rs
+++ Kernel/Modules/usb_hid/lib.rs
@@ -10,6 +10,8 @@ use kernel::prelude::*;
 extern crate kernel;
 extern crate usb_core;
 
+mod report_parser;
+
 module_define!{usb_hid, [usb_core], init}
 
 fn init()
@@ -65,6 +67,65 @@ impl ::usb_core::device::Driver for Driver
 	}
 }
 
+mod collection_parse
+{
+	use crate::report_parser::ParseState;
+
+	pub fn root() -> &'static dyn Handler {
+		&Root
+	}
+	pub trait Handler
+	{
+		fn parent(&self) -> &'static dyn Handler;
+		fn child(&self, state: &ParseState, num: u32) -> Option<&'static dyn Handler>;
+
+		fn input(&self, _state: &ParseState, _bits: u32) { }
+		fn output(&self, _state: &ParseState, _bits: u32) { }
+		fn feature(&self, _state: &ParseState, _bits: u32) { }
+	}
+	struct Root;
+	impl Handler for Root
+	{
+		fn parent(&self) -> &'static dyn Handler { &Root }
+		fn child(&self, state: &ParseState, num: u32) -> Option<&'static dyn Handler> {
+			match num
+			{
+			1 => match state.usage.get(0)
+				{
+				0x0001_0001 => None,	// "General Desktop" -> Pointer
+				0x0001_0002 => Some(&Mouse),	// "General Desktop" -> Mouse
+				0x0001_0004 => None,	// "General Desktop" -> Joystick
+				0x0001_0005 => None,	// "General Desktop" -> Game Pad
+				0x0001_0006 => None,	// "General Desktop" -> Keyboard
+				0x0007_0000 ..= 0x0007_FFFF => None,	// Keyboard/Keypad
+				_ => None,
+				},
+			_ => None,
+			}
+		}
+	}
+
+	struct Mouse;
+	impl Handler for Mouse
+	{
+		fn parent(&self) -> &'static dyn Handler { &Root }
+		fn child(&self, _state: &ParseState, _num: u32) -> Option<&'static dyn Handler> {
+			Some(&MouseInner)
+		}
+	}
+	struct MouseInner;
+	impl Handler for MouseInner
+	{
+		fn parent(&self) -> &'static dyn Handler { &Mouse }
+		fn child(&self, _state: &ParseState, _num: u32) -> Option<&'static dyn Handler> {
+			None
+		}
+		fn input(&self, _state: &ParseState, bits: u32) {
+			log_debug!("Mouse Input {:b} ({:?})", bits, _state);
+		}
+	}
+}
+
 impl Driver
 {
 	async fn start_device_inner(ep0: &::usb_core::ControlEndpoint, endpoints: Vec<::usb_core::Endpoint>, report_desc_len: u16)
@@ -76,30 +137,54 @@ impl Driver
 
 		// 2. Parse the report descriptor, and locate collections of known usage
 		// - Use collections to determine what bindings to set up
-		let mut state = ReportParseState::default();
-		for (id, val) in ReportIterRaw(&buf)
 		{
-			let op = ReportOp::from_pair(id, val);
-			log_debug!("> {:?}", op);
-			match op
+			let mut collection = collection_parse::root();
+			let mut collection_depth = 0;
+			let mut state = report_parser::ParseState::default();
+			for (id, val) in report_parser::IterRaw(&buf)
 			{
-			ReportOp::Collection(num) => {
-				},
-			ReportOp::EndCollection => {
-				//log_error!("TODO: USB HID start_device - coll={:?}", collection);
-				},
-			ReportOp::Input(v) => {
-				log_debug!("> INPUT {:09b} {:?}", v, state);
-				},
-			ReportOp::Output(v) => {
-				log_debug!("> OUTPUT {:09b} {:?}", v, state);
-				},
-			ReportOp::Feature(v) => {
-				log_debug!("> FEATURE {:09b} {:?}", v, state);
-				},
-			_ => {},
+				let op = report_parser::Op::from_pair(id, val);
+				log_debug!("> {:?}", op);
+				match op
+				{
+				report_parser::Op::Collection(num) => {
+					// Check the current collection state.
+					match collection.child(&state, num)
+					{
+					// if this number is known, update current state
+					Some(v) => collection = v,
+					// else, increment depth
+					None => collection_depth += 1,
+					}
+					},
+				report_parser::Op::EndCollection => {
+					// If depth is non-zero, decrement
+					if collection_depth > 0 {
+						collection_depth -= 1;
+					}
+					// else, go to current collection parent
+					else {
+						collection = collection.parent();
+					}
+					},
+				report_parser::Op::Input(v) => {
+					if collection_depth == 0 {
+						collection.input(&state, v);
+					}
+					else {
+						log_debug!("> INPUT {:09b} {:?}", v, state);
+					}
+					},
+				report_parser::Op::Output(v) => {
+					log_debug!("> OUTPUT {:09b} {:?}", v, state);
+					},
+				report_parser::Op::Feature(v) => {
+					log_debug!("> FEATURE {:09b} {:?}", v, state);
+					},
+				_ => {},
+				}
+				state.update(op);
 			}
-			state.update(op);
 		}
 
 		let mut int_endpoint = None;
@@ -121,13 +206,13 @@ impl Driver
 			//let mut bs = BitStream::new(&d);
 
 			// Decode input using the report descriptor
-			let mut state = ReportParseState::default();
-			for (id, val) in ReportIterRaw(&buf)
+			let mut state = report_parser::ParseState::default();
+			for (id, val) in report_parser::IterRaw(&buf)
 			{
-				let op = ReportOp::from_pair(id, val);
+				let op = report_parser::Op::from_pair(id, val);
 				match op
 				{
-				ReportOp::Input(_v) =>
+				report_parser::Op::Input(_v) =>
 					for i in 0 .. state.report_count as usize
 					{
 						let usage = state.usage.get(i);
@@ -138,285 +223,6 @@ impl Driver
 				state.update(op);
 			}
 
-		}
-	}
-}
-
-#[derive(Debug)]
-struct Input
-{
-	usage: u32,
-	bits: usize,
-	logical_range: ::core::ops::Range<i32>,
-	//physical_range: ::core::ops::Range<i32>,
-}
-
-/// Iterate over raw entries in a report descriptor
-struct ReportIterRaw<'a>(&'a [u8]);
-impl<'a> Iterator for ReportIterRaw<'a>
-{
-	type Item = (u8, u32);
-	fn next(&mut self) -> Option<Self::Item>
-	{
-		if self.0.len() == 0 {
-			None
-		}
-		else
-		{
-			fn mk_u32_le(b0: u8, b1: u8, b2: u8, b3: u8) -> u32 {
-				b0 as u32 | (b1 as u32) << 8 | (b2 as u32) << 16 | (b3 as u32) << 24
-			}
-			let op_byte = self.0[0];
-			let len;
-			let val = match op_byte & 3
-				{
-				0 => { len = 1; 0 },
-				1 => { len = 2; if self.0.len() < len { return None; } mk_u32_le(self.0[1], 0, 0, 0) },
-				2 => { len = 3; if self.0.len() < len { return None; } mk_u32_le(self.0[1], self.0[2], 0, 0) },
-				3 => { len = 5; if self.0.len() < len { return None; } mk_u32_le(self.0[1], self.0[2], self.0[3], self.0[4]) },
-				_ => unreachable!(),
-				};
-			if op_byte == 0xFC|2 {
-			}
-			self.0 = &self.0[len..];
-			Some( (op_byte, val) )
-		}
-	}
-}
-
-macro_rules! define_ops {
-	($name:ident : $($val:expr => $var:ident$(($t:ty))?,)*) => {
-		#[derive(Debug)]
-		enum $name
-		{
-			$($var$(($t))?, )*
-		}
-		
-		impl $name
-		{
-			fn from_pair(id: u8, val: u32) -> ReportOp
-			{
-				match id & 0xFC
-				{
-				$(
-				$val => $name::$var$((<$t>::from(val)))?,
-				)*
-				_ => $name::Unk(id, val),
-				}
-			}
-		}
-	};
-}
-
-#[derive(Debug)]
-enum ReportOp
-{
-	// --- (x0)
-	Input(u32),
-	Output(u32),
-	Collection(u32),
-	Feature(u32),
-	EndCollection,//(u32),
-
-	// --- Global items (x4)
-	UsagePage(u32),
-	LogicalMin(i32),
-	LogicalMax(i32),
-	PhysicalMin(i32),
-	PhysicalMax(i32),
-	UnitExponent(u32),
-	Unit(u32),
-	ReportSize(u32),
-	ReportId(u32),
-	ReportCount(u32),
-	Push,
-	Pop,
-
-	// --- Local items (x8)
-	UsageSingle(u32,bool),
-	UsageRangeStart(u32,bool),
-	UsageRangeEnd(u32,bool),
-	DesignatorSingle(u32),
-	DesignatorRangeStart(u32),
-	DesignatorRangeEnd(u32),
-	_Reserved(u32),
-	StringSingle(u32),
-	StringRangeStart(u32),
-	StringRangeEnd(u32),
-	Delimiter,
-	LongItem(u32),
-
-	Unk(u8, u32)
-}
-impl ReportOp
-{
-	fn from_pair(id: u8, val: u32) -> ReportOp
-	{
-		fn i32_se(v: u32, sz: u8) -> i32 {
-			let sign_bits = match sz
-				{
-				1 => if v >= 0x80 { !0x7F } else { 0 },
-				2 => if v >= 0x8000 { !0x7FFF } else { 0 },
-				_ => 0,
-				};
-			(v | sign_bits) as i32
-		}
-		match id & 0xFC
-		{
-		// --- (x0)
-		0x80 => ReportOp::Input(val),
-		0x90 => ReportOp::Output(val),
-		0xA0 => ReportOp::Collection(val),
-		0xB0 => ReportOp::Feature(val),
-		0xC0 => ReportOp::EndCollection,//(val),
-
-		// --- Global items (x4)
-		0x04 => ReportOp::UsagePage(val),
-		0x14 => ReportOp::LogicalMin(i32_se(val, id & 3)),
-		0x24 => ReportOp::LogicalMax(i32_se(val, id & 3)),
-		0x34 => ReportOp::PhysicalMin(i32_se(val, id & 3)),
-		0x44 => ReportOp::PhysicalMax(i32_se(val, id & 3)),
-		0x54 => ReportOp::UnitExponent(val),
-		0x64 => ReportOp::Unit(val),
-		0x74 => ReportOp::ReportSize(val),
-		0x84 => ReportOp::ReportId(val),
-		0x94 => ReportOp::ReportCount(val),
-		0xA4 => ReportOp::Push,
-		0xB4 => ReportOp::Pop,
-
-		// --- Local items (x8)
-		0x08 => ReportOp::UsageSingle(val, id & 0x3 == 3),
-		0x18 => ReportOp::UsageRangeStart(val, id & 0x3 == 3),
-		0x28 => ReportOp::UsageRangeEnd(val, id & 0x3 == 3),
-		0x38 => ReportOp::DesignatorSingle(val),
-		0x48 => ReportOp::DesignatorRangeStart(val),
-		0x58 => ReportOp::DesignatorRangeEnd(val),
-		0x68 => ReportOp::_Reserved(val),
-		0x78 => ReportOp::StringSingle(val),
-		0x88 => ReportOp::StringRangeStart(val),
-		0x98 => ReportOp::StringRangeEnd(val),
-		0xA8 => ReportOp::Delimiter,
-		0xFC => ReportOp::LongItem(val),
-		_ => ReportOp::Unk(id, val),
-		}
-	}
-}
-
-#[derive(Default,Debug)]
-struct ReportParseState
-{
-	//collection: Vec<u32>,
-	// Global
-	usage_page: u32,
-	logical_range: (Option<i32>,Option<i32>),
-	physical_range: (Option<i32>,Option<i32>),
-	unit_exponent: Option<u32>,
-	unit: Option<u32>,
-
-	report_size: u32,
-	report_id: Option<u32>,
-	report_count: u32,
-
-	// Local, cleared after the next main
-	usage: List,
-	designator: List,
-	string: List,
-}
-#[derive(Debug)]
-enum List
-{
-	Unset,
-	Single(u32),
-	ProtoRange(u32),
-	Range(u32, u32),
-}
-impl Default for List {
-	fn default() -> Self { List::Unset }
-}
-impl List
-{
-	fn set_single(&mut self, v: u32) {
-		*self = List::Single(v);
-	}
-	fn set_start(&mut self, v: u32) {
-		*self = List::ProtoRange(v);
-	}
-	fn set_end(&mut self, v: u32) {
-		match *self
-		{
-		List::ProtoRange(s) => {
-			*self = List::Range(s, v);
-			},
-		_ => {},
-		}
-	}
-	fn get(&self, idx: usize) -> u32 {
-		match *self
-		{
-		List::Unset => 0,
-		List::Single(v) => v,
-		List::ProtoRange(_v) => 0,
-		List::Range(s,e) => {
-			if (e - s) as usize <= idx {
-				s + idx as u32
-			}
-			else {
-				e
-			}
-			},
-		}
-	}
-}
-impl ReportParseState
-{
-	fn clear_local(&mut self)
-	{
-		self.usage = Default::default();
-		self.designator = Default::default();
-		self.string = Default::default();
-	}
-	fn update(&mut self, op: ReportOp)
-	{
-		match op
-		{
-		ReportOp::Input(_) => { self.clear_local(); },
-		ReportOp::Output(_) => { self.clear_local(); },
-		ReportOp::Feature(_) => { self.clear_local(); },
-		//ReportOp::Collection(v) => self.collection.push(v),
-		//ReportOp::EndCollection => { self.collection.pop(); },
-		ReportOp::Collection(_) => {},
-		ReportOp::EndCollection => {},
-
-		ReportOp::UsagePage(v) => self.usage_page = v << 16,
-		ReportOp::LogicalMin(v) => self.logical_range.0 = Some(v),
-		ReportOp::LogicalMax(v) => self.logical_range.1 = Some(v),
-		ReportOp::PhysicalMin(v) => self.physical_range.0 = Some(v),
-		ReportOp::PhysicalMax(v) => self.physical_range.1 = Some(v),
-		ReportOp::UnitExponent(v) => self.unit_exponent = Some(v),
-		ReportOp::Unit(v) => self.unit = Some(v),
-		ReportOp::ReportSize(v) => self.report_size = v,
-		ReportOp::ReportId(v) => self.report_id = Some(v),
-		ReportOp::ReportCount(v) => self.report_count = v,
-
-		ReportOp::Push => todo!("push"),
-		ReportOp::Pop => todo!("pop"),
-
-		ReportOp::UsageSingle(v,is32) => self.usage.set_single( if is32 { 0 } else { self.usage_page } | v),
-		ReportOp::UsageRangeStart(v,is32) => self.usage.set_start(if is32 { 0 } else { self.usage_page } | v),
-		ReportOp::UsageRangeEnd(v,is32) => self.usage.set_end(if is32 { 0 } else { self.usage_page } | v),
-
-		ReportOp::DesignatorSingle(v) => self.designator.set_single(v),
-		ReportOp::DesignatorRangeStart(v) => self.designator.set_start(v),
-		ReportOp::DesignatorRangeEnd(v) => self.designator.set_end(v),
-
-		ReportOp::StringSingle(v) => self.string.set_single(v),
-		ReportOp::StringRangeStart(v) => self.string.set_start(v),
-		ReportOp::StringRangeEnd(v) => self.string.set_end(v),
-
-		ReportOp::Delimiter => todo!("Delimiter"),
-		ReportOp::_Reserved(..) => {},
-		ReportOp::LongItem(..) => {},
-		ReportOp::Unk(..) => {},
 		}
 	}
 }
---
+++ Kernel/Modules/usb_hid/report_parser.rs
@@ -0,0 +1,246 @@
+
+/// Iterate over raw entries in a report descriptor
+pub struct IterRaw<'a>(pub &'a [u8]);
+impl<'a> Iterator for IterRaw<'a>
+{
+	type Item = (u8, u32);
+	fn next(&mut self) -> Option<Self::Item>
+	{
+		if self.0.len() == 0 {
+			None
+		}
+		else
+		{
+			fn mk_u32_le(b0: u8, b1: u8, b2: u8, b3: u8) -> u32 {
+				b0 as u32 | (b1 as u32) << 8 | (b2 as u32) << 16 | (b3 as u32) << 24
+			}
+			let op_byte = self.0[0];
+			let len;
+			let val = match op_byte & 3
+				{
+				0 => { len = 1; 0 },
+				1 => { len = 2; if self.0.len() < len { return None; } mk_u32_le(self.0[1], 0, 0, 0) },
+				2 => { len = 3; if self.0.len() < len { return None; } mk_u32_le(self.0[1], self.0[2], 0, 0) },
+				3 => { len = 5; if self.0.len() < len { return None; } mk_u32_le(self.0[1], self.0[2], self.0[3], self.0[4]) },
+				_ => unreachable!(),
+				};
+			if op_byte == 0xFC|2 {
+			}
+			self.0 = &self.0[len..];
+			Some( (op_byte, val) )
+		}
+	}
+}
+
+#[derive(Debug)]
+pub enum Op
+{
+	// --- (x0)
+	Input(u32),
+	Output(u32),
+	Collection(u32),
+	Feature(u32),
+	EndCollection,//(u32),
+
+	// --- Global items (x4)
+	UsagePage(u32),
+	LogicalMin(i32),
+	LogicalMax(i32),
+	PhysicalMin(i32),
+	PhysicalMax(i32),
+	UnitExponent(u32),
+	Unit(u32),
+	ReportSize(u32),
+	ReportId(u32),
+	ReportCount(u32),
+	Push,
+	Pop,
+
+	// --- Local items (x8)
+	UsageSingle(u32,bool),
+	UsageRangeStart(u32,bool),
+	UsageRangeEnd(u32,bool),
+	DesignatorSingle(u32),
+	DesignatorRangeStart(u32),
+	DesignatorRangeEnd(u32),
+	_Reserved(u32),
+	StringSingle(u32),
+	StringRangeStart(u32),
+	StringRangeEnd(u32),
+	Delimiter,
+	LongItem(u32),
+
+	Unk(u8, u32)
+}
+impl Op
+{
+	pub fn from_pair(id: u8, val: u32) -> Op
+	{
+		fn i32_se(v: u32, sz: u8) -> i32 {
+			let sign_bits = match sz
+				{
+				1 => if v >= 0x80 { !0x7F } else { 0 },
+				2 => if v >= 0x8000 { !0x7FFF } else { 0 },
+				_ => 0,
+				};
+			(v | sign_bits) as i32
+		}
+		match id & 0xFC
+		{
+		// --- (x0)
+		0x80 => Op::Input(val),
+		0x90 => Op::Output(val),
+		0xA0 => Op::Collection(val),
+		0xB0 => Op::Feature(val),
+		0xC0 => Op::EndCollection,//(val),
+
+		// --- Global items (x4)
+		0x04 => Op::UsagePage(val),
+		0x14 => Op::LogicalMin(i32_se(val, id & 3)),
+		0x24 => Op::LogicalMax(i32_se(val, id & 3)),
+		0x34 => Op::PhysicalMin(i32_se(val, id & 3)),
+		0x44 => Op::PhysicalMax(i32_se(val, id & 3)),
+		0x54 => Op::UnitExponent(val),
+		0x64 => Op::Unit(val),
+		0x74 => Op::ReportSize(val),
+		0x84 => Op::ReportId(val),
+		0x94 => Op::ReportCount(val),
+		0xA4 => Op::Push,
+		0xB4 => Op::Pop,
+
+		// --- Local items (x8)
+		0x08 => Op::UsageSingle(val, id & 0x3 == 3),
+		0x18 => Op::UsageRangeStart(val, id & 0x3 == 3),
+		0x28 => Op::UsageRangeEnd(val, id & 0x3 == 3),
+		0x38 => Op::DesignatorSingle(val),
+		0x48 => Op::DesignatorRangeStart(val),
+		0x58 => Op::DesignatorRangeEnd(val),
+		0x68 => Op::_Reserved(val),
+		0x78 => Op::StringSingle(val),
+		0x88 => Op::StringRangeStart(val),
+		0x98 => Op::StringRangeEnd(val),
+		0xA8 => Op::Delimiter,
+		0xFC => Op::LongItem(val),
+		_ => Op::Unk(id, val),
+		}
+	}
+}
+
+#[derive(Default,Debug)]
+pub struct ParseState
+{
+	// Global
+	pub usage_page: u32,
+	pub logical_range: (Option<i32>,Option<i32>),
+	pub physical_range: (Option<i32>,Option<i32>),
+	pub unit_exponent: Option<u32>,
+	pub unit: Option<u32>,
+
+	pub report_size: u32,
+	pub report_id: Option<u32>,
+	pub report_count: u32,
+
+	// Local, cleared after the next main
+	pub usage: List,
+	pub designator: List,
+	pub string: List,
+}
+#[derive(Debug)]
+pub enum List
+{
+	Unset,
+	Single(u32),
+	ProtoRange(u32),
+	Range(u32, u32),
+}
+impl Default for List {
+	fn default() -> Self { List::Unset }
+}
+impl List
+{
+	fn set_single(&mut self, v: u32) {
+		*self = List::Single(v);
+	}
+	fn set_start(&mut self, v: u32) {
+		*self = List::ProtoRange(v);
+	}
+	fn set_end(&mut self, v: u32) {
+		match *self
+		{
+		List::ProtoRange(s) => {
+			*self = List::Range(s, v);
+			},
+		_ => {},
+		}
+	}
+	pub fn get(&self, idx: usize) -> u32 {
+		match *self
+		{
+		List::Unset => 0,
+		List::Single(v) => v,
+		List::ProtoRange(_v) => 0,
+		List::Range(s,e) => {
+			if (e - s) as usize <= idx {
+				s + idx as u32
+			}
+			else {
+				e
+			}
+			},
+		}
+	}
+}
+impl ParseState
+{
+	fn clear_local(&mut self)
+	{
+		self.usage = Default::default();
+		self.designator = Default::default();
+		self.string = Default::default();
+	}
+	pub fn update(&mut self, op: Op)
+	{
+		match op
+		{
+		Op::Input(_) => { self.clear_local(); },
+		Op::Output(_) => { self.clear_local(); },
+		Op::Feature(_) => { self.clear_local(); },
+		//Op::Collection(v) => self.collection.push(v),
+		//Op::EndCollection => { self.collection.pop(); },
+		Op::Collection(_) => {},
+		Op::EndCollection => {},
+
+		Op::UsagePage(v) => self.usage_page = v << 16,
+		Op::LogicalMin(v) => self.logical_range.0 = Some(v),
+		Op::LogicalMax(v) => self.logical_range.1 = Some(v),
+		Op::PhysicalMin(v) => self.physical_range.0 = Some(v),
+		Op::PhysicalMax(v) => self.physical_range.1 = Some(v),
+		Op::UnitExponent(v) => self.unit_exponent = Some(v),
+		Op::Unit(v) => self.unit = Some(v),
+		Op::ReportSize(v) => self.report_size = v,
+		Op::ReportId(v) => self.report_id = Some(v),
+		Op::ReportCount(v) => self.report_count = v,
+
+		Op::Push => todo!("push"),
+		Op::Pop => todo!("pop"),
+
+		Op::UsageSingle(v,is32) => self.usage.set_single( if is32 { 0 } else { self.usage_page } | v),
+		Op::UsageRangeStart(v,is32) => self.usage.set_start(if is32 { 0 } else { self.usage_page } | v),
+		Op::UsageRangeEnd(v,is32) => self.usage.set_end(if is32 { 0 } else { self.usage_page } | v),
+
+		Op::DesignatorSingle(v) => self.designator.set_single(v),
+		Op::DesignatorRangeStart(v) => self.designator.set_start(v),
+		Op::DesignatorRangeEnd(v) => self.designator.set_end(v),
+
+		Op::StringSingle(v) => self.string.set_single(v),
+		Op::StringRangeStart(v) => self.string.set_start(v),
+		Op::StringRangeEnd(v) => self.string.set_end(v),
+
+		Op::Delimiter => todo!("Delimiter"),
+		Op::_Reserved(..) => {},
+		Op::LongItem(..) => {},
+		Op::Unk(..) => {},
+		}
+	}
+}
+
