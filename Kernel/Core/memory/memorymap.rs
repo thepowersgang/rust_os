4a217619de43034ed7beea8c50b09b70bc03baa5
John Hodge <tpg@mutabah.net>
2021-04-24 10:57:16 +0800

    Kernel - Fix MemoryMapBuilder bugs

--- Kernel/Core/memory/memorymap.rs
+++ Kernel/Core/memory/memorymap.rs
@@ -9,11 +9,15 @@ use prelude::*;
 #[derive(PartialEq,Debug,Copy,Clone)]
 pub enum MemoryState
 {
+	/// Reserved for use by the firmware
 	Reserved,
+	/// Used by the kernel already
 	Used,
+	/// Free for use
 	Free,
 }
 
+/// A constant default/padding map entry
 pub const MAP_PAD: MemoryMapEnt = MemoryMapEnt {
 	start: 0,
 	size: 0,
@@ -21,15 +25,21 @@ pub const MAP_PAD: MemoryMapEnt = MemoryMapEnt {
 	domain: 0
 	};
 
+/// Memory map entry
 #[derive(Copy,Clone)]
 pub struct MemoryMapEnt
 {
+	/// First address in the map
 	pub start: u64,
+	/// Size of the memory
 	pub size: u64,
+	/// Memory class
 	pub state: MemoryState,
+	/// NUMA domain for this memory
 	pub domain: u16,
 }
 
+/// Helper to handle building a memory map (by masking out segments)
 pub struct MemoryMapBuilder<'buf>
 {
 	slots: &'buf mut [MemoryMapEnt],
@@ -54,6 +64,7 @@ impl ::core::fmt::Debug for MemoryMapEnt
 
 impl<'buf> MemoryMapBuilder<'buf>
 {
+	/// Construct a new builder using the provided backing memory
 	pub fn new<'a>(buf: &'a mut [MemoryMapEnt]) -> MemoryMapBuilder<'a>
 	{
 		MemoryMapBuilder {
@@ -61,11 +72,13 @@ impl<'buf> MemoryMapBuilder<'buf>
 			size: 0,
 		}
 	}
+	/// Currently populated size of the map
 	pub fn size(&self) -> usize
 	{
 		self.size
 	}
 	
+	/// Add a new (base) entry to the map
 	pub fn append(&mut self, base: u64, size: u64, state: MemoryState, domain: u16)
 	{
 		log_debug!("append(base={:#x}, size={:#x}, state={:?}, domain={})",
@@ -79,26 +92,12 @@ impl<'buf> MemoryMapBuilder<'buf>
 		self.size += 1;
 	}
 	
+	/// Sort all entries
 	pub fn sort(&mut self)
 	{
-		for entid in 0 .. self.size
-		{
-			let mut sel = entid;
-			for tgt in entid+1 .. self.size
-			{
-				if self.slots[sel].start > self.slots[tgt].start {
-					sel = tgt;
-				}
-			}
-			if sel != entid
-			{
-				// Swap
-				let a = self.slots[sel];
-				self.slots[sel] = self.slots[entid];
-				self.slots[entid] = a;
-			}
-		}
+		self.slots[..self.size].sort_unstable_by_key(|v| v.start);
 	}
+	/// Validate the memory map's consistency (no overlapping entries)
 	pub fn validate(&self) -> bool
 	{
 		let mut ret = true;
@@ -113,7 +112,67 @@ impl<'buf> MemoryMapBuilder<'buf>
 		}
 		ret
 	}
+	/// Compact the map (combine abutting entries)
+	pub fn compact(&mut self)
+	{
+		self.sort();
+		let mut it = self.slots[..self.size].iter_mut();
+		if let Some(mut cur) = it.next()
+		{
+			for next in it
+			{
+				let mut advance = true;
+				// If the current overlaps with or is abuts next entry 
+				if cur.start + cur.size >= next.start
+				{
+					let overlap = (cur.start + cur.size) - next.start;
+					if cur.state == next.state && cur.domain == next.domain {
+						// Potentially overlapping, and same - merge
+						if next.size >= overlap {
+							cur.size += next.size - overlap;
+						}
+						next.size = 0;
+						advance = false;
+					}
+					else if overlap > 0 {
+						// Overlapping and different!
+						// - Print and error
+					}
+					else {
+						// Adjacent and different
+					}
+				}
+				if advance
+				{
+					cur = next;
+				}
+			}
+		}
+		//log_trace!("compact: {:?}", &self.slots[..self.size]);
+
+		// Remove all zero-sized entries
+		let mut w_i = 1;
+		for r_i in 1 .. self.size
+		{
+			if self.slots[r_i].size == 0
+			{
+				// Ignore
+			}
+			else
+			{
+				if r_i > w_i
+				{
+					self.slots[w_i] = self.slots[r_i];
+				}
+				w_i += 1;
+			}
+		}
+		self.size = w_i;
+		//log_trace!("compact: {:?}", &self.slots[..self.size]);
+	}
 	
+	/// Update the state of a specified range of memory
+	/// NOTE: This rounds all actions to page boundaries
 	pub fn set_range(&mut self, base_: u64, size_: u64, state: MemoryState, domain: u16) -> Result<(),()>
 	{
 		log_debug!("set_range(base={:#x}, size={:#x}, state={:?}, domain={})",
@@ -137,25 +196,33 @@ impl<'buf> MemoryMapBuilder<'buf>
 			return Ok( () );
 		}
 		
+		let first = pos;
 		while pos < self.size && size > 0 {
-			let slot = self.slots[pos];
+			let mut slot = self.slots[pos];
 			log_debug!("{}: ({:#x}+{:#x}): slot={:?}", pos, base, size, slot);
 			if slot.start > base {
+				// If this slot starts after the base, then consume enough to set `base` to the start
 				let seg_size = ::core::cmp::min(size, slot.start - base);
 				base += seg_size;
 				size -= seg_size;
 			}
 			else {
-				let seg_size = ::core::cmp::min(size, slot.size - (base - slot.start));
+				let slot_ofs = base - slot.start;
+				let seg_size = ::core::cmp::min(size, slot.size - slot_ofs);
 				if slot.state != state {
 					// Altered region starts within block
+					// - Split that block, advance `pos`, and then do other checks
 					if slot.start < base {
 						let leftsize = base - slot.start;
-						try!(self._split_at(pos, leftsize));
+						self._split_at(pos, leftsize)?;
+						pos += 1;
+						slot = self.slots[pos];
 					}
+
 					// Altered region ends within block
-					else if slot.size > size {
-						try!( self._split_at(pos, size) );
+					// - Split the block
+					if slot.size > size {
+						self._split_at(pos, size)?;
 						base += size;
 						size = 0;
 					}
@@ -164,6 +231,7 @@ impl<'buf> MemoryMapBuilder<'buf>
 						base += seg_size;
 						size -= seg_size;
 					}
+					// Update the state of the entire block (known to be equal to the current range)
 					self.slots[pos].state = state;
 					self.slots[pos].domain = domain;
 				}
@@ -175,12 +243,19 @@ impl<'buf> MemoryMapBuilder<'buf>
 				pos += 1;
 			}
 		}
-		// TODO: Merge ajoining regions
-		//self.compact();
+		// Print all of the updated slots
+		for pos in first..pos
+		{
+			log_debug!("={}: slot={:?}", pos, self.slots[pos]);
+		}
+
+		// Merge ajoining regions
+		self.compact();
 	
 		Ok( () )
 	}
 	
+	/// Locate the slot for a given address
 	fn _find_addr(&self, addr: u64) -> usize
 	{
 		for (i,slot) in self.slots[0 .. self.size].iter().enumerate()
@@ -192,6 +267,7 @@ impl<'buf> MemoryMapBuilder<'buf>
 		return self.size
 	}
 	
+	/// Split the given slot into two equivalent slots
 	fn _split_at(&mut self, index: usize, left_size: u64) -> Result<(),()>
 	{
 		if self.size >= self.slots.len()
