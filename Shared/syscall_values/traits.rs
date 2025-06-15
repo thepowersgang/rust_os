a328ca0fea4d01189f0243799863cab7e2ed07fa
John Hodge <tpg@mutabah.net>
2025-06-15 10:22:51 +0800

    syscalls - Fix nightly breakage with const-generic trickery

--- Shared/syscall_values/traits.rs
+++ Shared/syscall_values/traits.rs
@@ -14,10 +14,15 @@ impl ToUsizeArray for u32 { const LEN: usize = 1; fn into_array(self) -> [usize;
 impl ToUsizeArray for u16 { const LEN: usize = 1; fn into_array(self) -> [usize; 1] { [self as _] } }
 impl ToUsizeArray for u8  { const LEN: usize = 1; fn into_array(self) -> [usize; 1] { [self as _] } }
 impl ToUsizeArray for bool { const LEN: usize = 1; fn into_array(self) -> [usize; 1] { [self as _] } }
-impl<'a, T: 'static> ToUsizeArray for &'a T     { const LEN: usize = 1; fn into_array(self) -> [usize; 1] { [self as *const _ as usize] } }
-impl<'a, T: 'static> ToUsizeArray for &'a mut T { const LEN: usize = 1; fn into_array(self) -> [usize; 1] { [self as *const _ as usize] } }
-impl<'a, T: 'static> ToUsizeArray for &'a [T]     { const LEN: usize = 2; fn into_array(self) -> [usize; 2] { [self.as_ptr() as usize, self.len()] } }
-impl<'a, T: 'static> ToUsizeArray for &'a mut [T] { const LEN: usize = 2; fn into_array(self) -> [usize; 2] { [self.as_ptr() as usize, self.len()] } }
+// NOTE: The `transmute_copy` here is a workaround for breakage to const generics
+// SAFE: Valid copying transmute (sizes should match)
+impl<'a, T: 'static> ToUsizeArray for &'a T     { const LEN: usize = 1; fn into_array(self) -> [usize; Self::LEN] { unsafe { ::core::mem::transmute_copy(&[self as *const _ as usize]) } } }
+// SAFE: Valid copying transmute (sizes should match)
+impl<'a, T: 'static> ToUsizeArray for &'a mut T { const LEN: usize = 1; fn into_array(self) -> [usize; Self::LEN] { unsafe { ::core::mem::transmute_copy(&[self as *const _ as usize]) } } }
+// SAFE: Valid copying transmute (sizes should match)
+impl<'a, T: 'static> ToUsizeArray for &'a [T]     { const LEN: usize = 2; fn into_array(self) -> [usize; Self::LEN] { unsafe { ::core::mem::transmute_copy(&[self.as_ptr() as usize, self.len()]) } } }
+// SAFE: Valid copying transmute (sizes should match)
+impl<'a, T: 'static> ToUsizeArray for &'a mut [T] { const LEN: usize = 2; fn into_array(self) -> [usize; Self::LEN] { unsafe { ::core::mem::transmute_copy(&[self.as_ptr() as usize, self.len()]) } } }
 impl<'a> ToUsizeArray for &'a str { const LEN: usize = 2; fn into_array(self) -> [usize; 2] { [self.as_ptr() as usize, self.len()] } }
 impl ToUsizeArray for super::FixedStr8 { const LEN: usize = u64::LEN; fn into_array(self) -> [usize; Self::LEN] { <u64 as ToUsizeArray>::into_array(self.into()) } }
 #[cfg(target_pointer_width="64")]
